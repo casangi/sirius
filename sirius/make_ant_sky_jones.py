5f41efea54844b992d2f5d425dca020cec6d9ea0
Jan-Willem <jwsteeb@gmail.com>
2021-07-08 10:16:38 -0400

    remove pb

--- sirius/make_ant_sky_jones.py
+++ sirius/make_ant_sky_jones.py
@@ -15,7 +15,6 @@
 import numpy as np
 c = 299792458
 from ._sirius_utils._direction_rotate import _calc_rotation_mats, _cs_calc_rotation_mats
-from ._sirius_utils._make_pb_symmetric import _airy_disk, _casa_airy_disk
 from ._sirius_utils._apply_primary_beam import _apply_casa_airy_pb
 from ._sirius_utils._ant_jones_term import _calc_ant_jones
 import itertools
