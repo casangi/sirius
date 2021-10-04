7537f85ce58611ade15ff8c76f76112a18db013f
Jan-Willem Steeb <jsteeb@nrao.edu>
2021-10-04 19:19:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', "r") as fid:   #encoding='utf-8'
 
 setup(
     name='sirius',
-    version='0.0.6',
+    version='0.0.7',
     description='Simulation of Radio Interferometry from Unique Sources',
     long_description=long_description,
     long_description_content_type="text/markdown",
