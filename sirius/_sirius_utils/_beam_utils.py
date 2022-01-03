Fix beam max radius frequency scaling.

sirius/_sirius_utils/_beam_utils.py
@@ -28,7 +28,6 @@ from sirius_data._constants import map_mueler_to_pol, c
 
 
 def _calc_resolution(pb_freq,dish_diameter,beam_parms):
-    c = 299792458
     fov = beam_parms['fov_scaling']*(1.22 * c / (dish_diameter*pb_freq))
     min_delta = min(min(fov/beam_parms['image_size'][0]),min(fov/beam_parms['image_size'][1]))
     return min_delta
@@ -98,7 +97,7 @@ def _calc_pb_scale(flux, sep1, sep2, bm1_indx,bm2_indx,bm1_type,bm2_type,lmn1,lm
     if (bm1_indx == bm2_indx) and (bm1_type == bm2_type) and ~do_pointing: #Antennas are the same
         if bm1_type == 0: #Checks if it is a zernike model
             bm1 = beam_models_type0[bm1_indx]
-            max_rad = bm1[7]*freq/10**9 # scale max_rad_1GHz to freq
+            max_rad = bm1[7]/(freq/10**9) # scale max_rad_1GHz to freq
             
             if sep1 < max_rad:
                 #bm1 = beam_models_type0[bm1_indx]
@@ -113,7 +112,7 @@ def _calc_pb_scale(flux, sep1, sep2, bm1_indx,bm2_indx,bm1_type,bm2_type,lmn1,lm
         else:
             #bm1 = beam_models_type1[bm1_indx]
             bm1 = beam_models_type1[bm1_indx]
-            max_rad = bm1[4]*freq/10**9 # scale max_rad_1GHz to freq
+            max_rad = bm1[4]/(freq/10**9) # scale max_rad_1GHz to freq
             if sep1 < max_rad:
                 J_sampled = _sample_J_analytic(bm1[1],bm1[2],bm1[3],max_rad,lmn1,freq,1)
                 #print('J_sampled',J_sampled,bm1[1],bm1[2],bm1[3],lmn1,freq,1)
@@ -123,28 +122,28 @@ def _calc_pb_scale(flux, sep1, sep2, bm1_indx,bm2_indx,bm1_type,bm2_type,lmn1,lm
     else:
         if bm1_type == 0:
             bm1 = beam_models_type0[bm1_indx]
-            max_rad = bm1[7]*freq/10**9 # scale max_rad_1GHz to freq
+            max_rad = bm1[7]/(freq/10**9) # scale max_rad_1GHz to freq
             if sep1 < max_rad:
                 J_sampled1 = _sample_J(bm1[1],bm1[2],bm1[3],bm1[4],bm1[5],bm1[6],pa,freq,lmn1)[:,0]
             else:
                 outside_beam = True
         else:
             bm1 = beam_models_type1[bm1_indx]
-            max_rad = bm1[4]*freq/10**9 # scale max_rad_1GHz to freq
+            max_rad = bm1[4]/(freq/10**9) # scale max_rad_1GHz to freq
             if sep1 < max_rad:
                 J_sampled1 = _sample_J_analytic(bm1[1],bm1[2],bm1[3],max_rad,lmn1,freq,1)
             else:
                 outside_beam = True
         if bm2_type == 0:
             bm2 = beam_models_type0[bm2_indx]
-            max_rad = bm2[7]*freq/10**9 # scale max_rad_1GHz to freq
+            max_rad = bm2[7]/(freq/10**9) # scale max_rad_1GHz to freq
             if sep2 < max_rad:
                 J_sampled2 = _sample_J(bm2[1],bm2[2],bm2[3],bm2[4],bm2[5],bm2[6],pa,freq,lmn2)[:,0]
             else:
                 outside_beam =  True
         else:
             bm2 = beam_models_type1[bm2_indx]
-            max_rad = bm2[4]*freq/10**9 # scale max_rad_1GHz to freq
+            max_rad = bm2[4]/(freq/10**9) # scale max_rad_1GHz to freq
             if sep2 < max_rad:
                 J_sampled2 = _sample_J_analytic(bm2[1],bm2[2],bm2[3],max_rad,lmn2,freq,1)
             else:
@@ -292,7 +291,6 @@ def _make_mueler_mat(J1, J2, mueller_selection):
 def _calc_ant_jones(zpc_dataset,j_freq,j_pa,beam_parms):
     pa_prev = -42.0
     freq_prev = -42.0
-    c = 299792458
     
     j_planes = np.zeros((len(j_pa),len(j_freq),len(beam_parms['needed_pol']),beam_parms['image_size'][0],beam_parms['image_size'][1]),np.complex128)
     j_planes_shape = j_planes.shape

sirius/_sirius_utils/_primary_beam_funcs.py
@@ -28,26 +28,24 @@ r_vla_max = 0.8564*np.pi/180 #in radians
 #@jit(nopython=True,cache=True,nogil=True)
 @jit(nopython=True,nogil=True)
 def _casa_airy_pb(lmn,freq_chan,dish_diameter, blockage_diameter, ipower, r_max=r_vla_max, n_sample=10000):
+    #dish_diameter = pb_parms['dish_diameter']
+    #blockage_diameter = pb_parms['blockage_diameter']
+    #ipower = pb_parms['ipower']
     
-    if (lmn[0] != 0) or (lmn[1] != 0):
-        #dish_diameter = pb_parms['dish_diameter']
-        #blockage_diameter = pb_parms['blockage_diameter']
-        #ipower = pb_parms['ipower']
-        
-        k = (2*np.pi*freq_chan)/c
-        aperture = dish_diameter/2
+    #print(lmn,freq_chan,dish_diameter, blockage_diameter, ipower, r_max, n_sample)
+    
+    k = (2*np.pi*freq_chan)/c
+    aperture = dish_diameter/2
 
-        if n_sample is not None:
-            r = np.arcsin(np.sqrt(lmn[0]**2 + lmn[1]**2)) #CASA in PBMATH does a small angle approximation.
-            r_inc = ((r_max)/(n_sample-1))
-            r = (int(np.floor(r/r_inc + 0.5))*r_inc)*aperture*k
-            r = r*casa_twiddle
-        else:
-            r = np.arcsin(np.sqrt(lmn[0]**2 + lmn[1]**2)*k*aperture)   
+    if n_sample is not None:
+        r = np.arcsin(np.sqrt(lmn[0]**2 + lmn[1]**2)) #CASA in PBMATH does a small angle approximation.
+        r_inc = ((r_max)/(n_sample-1))
+        r = (int(np.floor(r/r_inc + 0.5))*r_inc)*aperture*k
+        r = r*casa_twiddle
+    else:
+        r = np.arcsin(np.sqrt(lmn[0]**2 + lmn[1]**2)*k*aperture)
         
-        #r = 2.0*np.arcsin(np.sqrt(np.sum(lmn**2))/2.0)*k*aperture # use lmn_rot in calc vis
-        #print('r',r)
-        
+    if (r != 0):
         if blockage_diameter==0.0:
             return (2.0*j1(r)/r)**ipower
         else:

sirius/calc_vis.py
@@ -18,12 +18,13 @@ from numba import jit
 import numba
 from ._sirius_utils._coord_transforms import _calc_rotation_mats, _directional_cosine,  _sin_project
 from ._sirius_utils._beam_utils import _calc_pb_scale, _beam_models_to_tuple, _pol_code_to_index
+from ._sirius_utils._array_utils import _is_subset
 from sirius_data._constants import map_mueler_to_pol, c
-
+from ._parm_utils._check_parms import _check_parms
 #def calc_vis(uvw,vis_data_shape,point_source_flux,point_source_ra_dec,pointing_ra_dec,phase_center_ra_dec,antenna1,antenna2,freq_chan,beam_model_map,beam_models, parallactic_angle, pol, mueller_selection):
 #    return 0
 
-def calc_vis_chunk(uvw,vis_data_shape, point_source_flux,point_source_ra_dec,pointing_ra_dec,phase_center_ra_dec,antenna1,antenna2,chan_chunk,beam_model_map,beam_models, parallactic_angle, pol, mueller_selection):
+def calc_vis_chunk(uvw,vis_data_shape, point_source_flux,point_source_ra_dec,pointing_ra_dec,phase_center_ra_dec,antenna1,antenna2,chan_chunk,beam_model_map,beam_models, parallactic_angle, pol, mueller_selection, check_parms=True):
     """
     Simulate interferometric visibilities.
     
@@ -50,7 +51,7 @@ def calc_vis_chunk(uvw,vis_data_shape, point_source_flux,point_source_ra_dec,poi
     beam_model_map: int np.array, [n_ant]
         Each element in beam_model_map is an index into beam_models.
     beam_models: list
-        List of beam models to use. Beam models can be any combination of function parameter dictionaries or image xr.Datasets. Any Zernike polynomial coefficient xr.Datasets models must be converted to images.
+        List of beam models to use. Beam models can be any combination of function parameter dictionaries or image xr.Datasets. Any Zernike polynomial coefficient xr.Datasets models must be converted to images using sirius.calc_beam.evaluate_beam_models.
     parallactic_angle: float np.array, [n_time], radians
         Parallactic angle over time at the array center.
     pol: int np.array 
@@ -58,11 +59,42 @@ def calc_vis_chunk(uvw,vis_data_shape, point_source_flux,point_source_ra_dec,poi
     mueller_selection: int np.array
         The elements in the 4x4 beam Mueller matrix to use. The elements are numbered row wise.
         For example [ 0, 5, 10, 15] are the diagonal elements.
+    check_parms: bool
+        Check input parameters and asign defaults.
+        
     Returns
     -------
     vis : complex np.array, [n_time,n_baseline,n_chan,n_pol]   
         Visibility data.
     """
+    
+    if check_parms:
+        n_time, n_baseline, n_chan, n_pol = vis_data_shape
+        n_ant = tel_xds.dims['ant_name']
+        
+        pol = np.array(pol)
+        assert(_is_subset(pol_codes_RL,pol) or _is_subset(pol_codes_XY,pol)), 'Pol selection invalid, must either be subset of [5,6,7,8] or [9,10,11,12] but is '
+        if not(_check_parms(beam_parms, 'mueller_selection', [list,np.array], list_acceptable_data_types=[np.int64], default = np.array([ 0, 5, 10, 15]),list_len=-1)): parms_passed = False
+        
+        #Check dimensions.
+        assert(point_source_flux.shape[0] == point_source_ra_dec.shape[1]), 'n_point_sources dimension of point_source_flux[' + str(point_source_flux.shape[0]) +'] and point_source_ra_dec['+str(point_source_ra_dec.shape[1])+'] do not match.'
+        assert(point_source_flux.shape[1] == 1) or (point_source_flux.shape[1] == n_time), 'n_time dimension in point_source_flux[' + str(point_source_flux.shape[1]) + '] must be either 1 or ' + str(n_time) + ' (see time_xda parameter).'
+        assert(point_source_flux.shape[2] == 1) or (point_source_flux.shape[2] == n_chan), 'n_chan dimension in point_source_flux[' + str(point_source_flux.shape[2]) + '] must be either 1 or ' + str(n_chan) + ' (see chan_xda parameter).'
+        assert(point_source_flux.shape[3] == 4), 'n_pol dimension in point_source_flux[' + str(point_source_flux.shape[3]) + '] must be 4.'
+        
+        assert(point_source_ra_dec.shape[0] == 1) or (point_source_ra_dec.shape[0] == n_time), 'n_time dimension in point_source_ra_dec[' + str(point_source_ra_dec.shape[0]) + '] must be either 1 or ' + str(n_time) + ' (see time_xda parameter).'
+        assert(point_source_ra_dec.shape[2] == 2), 'ra,dec dimension in point_source_ra_dec[' + str(point_source_ra_dec.shape[2]) + '] must be 2.'
+        
+        if pointing_ra_dec is not None:
+            assert(pointing_ra_dec.shape[0] == 1) or (pointing_ra_dec.shape[0] == n_time), 'n_time dimension in pointing_ra_dec[' + str(pointing_ra_dec.shape[0]) + '] must be either 1 or ' + str(n_time) + ' (see time_xda parameter).'
+            assert(pointing_ra_dec.shape[1] == 1) or (pointing_ra_dec.shape[1] == n_ant), 'n_ant dimension in pointing_ra_dec[' + str(pointing_ra_dec.shape[1]) + '] must be either 1 or ' + str(n_ant) + ' (see tel_xds.dims[\'ant_name\']).'
+            assert(pointing_ra_dec.shape[2] == 2), 'ra,dec dimension in pointing_ra_dec[' + str(pointing_ra_dec.shape[2]) + '] must be 2.'
+            
+            
+        assert(phase_center_ra_dec.shape[0] == 1) or (phase_center_ra_dec.shape[0] == n_time), 'n_time dimension in phase_center_ra_dec[' + str(phase_center_ra_dec.shape[0]) + '] must be either 1 or ' + str(n_time) + ' (see time_xda parameter).'
+        assert(phase_center_ra_dec.shape[1] == 2), 'ra,dec dimension in phase_center_ra_dec[' + str(phase_center_ra_dec.shape[1]) + '] must be 2.'
+            
+        assert(phase_center_names.shape[0] == 1) or (phase_center_names.shape[0] == n_time), 'n_time dimension in phase_center_ra_dec[' + str(phase_center_names.shape[0]) + '] must be either 1 or ' + str(n_time) + ' (see time_xda parameter).'
     
     # The _beam_models_to_tuple function is here to appease Numba the terrible. It unpacks the beam models from dictionaries and xr.Datasets to fixed tuples.
     beam_models_type0, beam_models_type1, beam_types, new_beam_model_map = _beam_models_to_tuple(beam_models,beam_model_map) 
@@ -176,5 +208,5 @@ def calc_vis_jit(vis_data,uvw,vis_data_shape,point_source_flux,point_source_ra_d
                         for i_pol in range(n_pol):
                             vis_data[i_time,i_baseline,i_chan,i_pol] = vis_data[i_time,i_baseline,i_chan,i_pol] + flux_scaled[pol[i_pol]]*np.exp(phase_scaled)/(1-lmn_rot[2])
                         #print("s4",time.time()-s4)
-    
+
 

sirius/simulation.py
@@ -368,19 +368,19 @@ def simulation_chunk(point_source_flux, point_source_ra_dec, pointing_ra_dec, ph
       
     t1 = time.time()
     #Evaluate zpc files
-    eval_beam_models, pa = evaluate_beam_models(beam_models,time_chunk,chan_chunk,phase_center_ra_dec,tel_xds.site_pos,beam_parms)
+    eval_beam_models, pa = evaluate_beam_models(beam_models,time_chunk,chan_chunk,phase_center_ra_dec,tel_xds.site_pos,beam_parms,check_parms=False)
     t1 = time.time()-t1
 
     #Calculate visibilities
     t2 = time.time()
     vis_data_shape =  np.concatenate((uvw.shape[0:2],[len(chan_chunk)],[len(pol)]))
-    vis =calc_vis_chunk(uvw,vis_data_shape,point_source_flux,point_source_ra_dec,pointing_ra_dec,phase_center_ra_dec,antenna1,antenna2,chan_chunk,beam_model_map,eval_beam_models, pa, pol, beam_parms['mueller_selection'])
+    vis =calc_vis_chunk(uvw,vis_data_shape,point_source_flux,point_source_ra_dec,pointing_ra_dec,phase_center_ra_dec,antenna1,antenna2,chan_chunk,beam_model_map,eval_beam_models, pa, pol, beam_parms['mueller_selection'],check_parms=False)
     t2 = time.time()-t2
 
     #Calculate and add noise
     t3 = time.time()
     if noise_parms is not None:
-        noise, weight, sigma = calc_a_noise_chunk(vis.shape,uvw,beam_model_map,eval_beam_models, antenna1, antenna2,noise_parms)
+        noise, weight, sigma = calc_a_noise_chunk(vis.shape,uvw,beam_model_map,eval_beam_models, antenna1, antenna2,noise_parms,check_parms=False)
         vis = vis + noise
     else:
         n_time, n_baseline, n_chan, n_pol = vis.shape