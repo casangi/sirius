2b95c9cee41dc7972c2ffe24d82a8c1a30eeb5e6
Jan-Willem Steeb <jsteeb@nrao.edu>
2022-01-06 19:13:52 -0500

    Add __version__ to sirius and outline of development section.

--- setup.py
+++ setup.py
@@ -2,10 +2,22 @@ from setuptools import setup, find_packages
 
 with open('README.md', "r") as fid:   #encoding='utf-8'
     long_description = fid.read()
+    
+#Load version. sirius/_version.py is the canonical place where the version number is stored.
+import re
+VERSIONFILE="sirius/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+################
 
 setup(
     name='sirius',
-    version='0.0.18',
+    version=verstr,
     description='Simulation of Radio Interferometry from Unique Sources',
     long_description=long_description,
     long_description_content_type="text/markdown",
