76c3b24830c8ac029e475d47493537c4c89e5c78
Jan-Willem Steeb <jsteeb@nrao.edu>
2022-02-08 16:14:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
                       'casatasks>=6.3.0.48',
                       'casadata>=2021.8.23',
                       'python-casacore>=3.4.0',
-                      'astropy>=5.0.1',
+                      'astropy>=4.3.1',
                       'cngi-prototype>=1.0.1'
                       ],
     extras_require={
